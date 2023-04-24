36062f64ff0b89cad833e12a7f1d5b3aac93e237
Gabriel <gabriel.jorge@estudiantes.utec.edu.uy>
2023-04-24 17:28:21 -0300

    First commit

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonCrud.Data;
+using PersonCrud.Models;
+
+namespace PersonCrud.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowView()
+        {
+            return View();
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Person p)
+        {
+
+            DBList.Add(p);
+
+            Console.WriteLine(DBList.Instance()[DBList.Instance().Count - 1].ToString());
+
+
+            return RedirectToAction("Create");
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Person p)
+        {
+            try
+            {
+                foreach (var item in DBList.Instance())
+                {
+                    if (item.Id == p.Id)
+                    {
+                        item.Document = p.Document;
+                        item.FirstName = p.FirstName;
+                        item.LastName = p.LastName;
+                        item.Birthdate = p.Birthdate;
+                        item.Married = p.Married;
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        //// POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                DBList.Delete(id);
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Data/DBList.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PersonCrud.Models;
+
+namespace PersonCrud.Data
+{
+    public class DBList
+    {
+        //singleton design creational pattern
+        private static List<Person> _peopleList = new List<Person>();
+
+        private DBList() { }
+
+        public static List<Person> Instance()
+        {
+            return _peopleList;
+        }
+        public static void Add(Person p)
+        {
+            _peopleList.Add(p);
+        }
+
+        public static void Delete(int id)
+        {
+            foreach(var person in _peopleList)
+            {
+                if (person.Id == id)
+                {
+                    _peopleList.Remove(person);
+                }
+                else
+                {
+                    throw new Exception("Person isn't in the DB.");
+                }
+            }
+        }
+    }
+}
